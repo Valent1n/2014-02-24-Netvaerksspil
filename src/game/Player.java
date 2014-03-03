4d5eeb069936dece4f18a9c46e95f00b47e7f845
Nick <nickandersen49@gmail.com>
2014-03-03 09:10:18 +0100

    ddff

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,53 +1,54 @@
 package game;
 
 public class Player {
-String name;
-int xpos;
-int ypos;
-int point;
-String direction;
+	String name;
+	int xpos;
+	int ypos;
+	int point;
+	String direction;
 
-public Player (String name) {
-	this.name = name;
-	xpos = 9;
-	ypos = 7;
-	point = 0;
-	direction = "up";
-}
+	public Player(String name) {
+		this.name = name;
+		xpos = 9;
+		ypos = 7;
+		point = 0;
+		direction = "up";
+	}
 
-public int getXpos() {
-	return xpos;
-}
+	public int getXpos() {
+		return xpos;
+	}
 
-public void setXpos(int xpos) {
-	this.xpos = xpos;
-}
+	public void setXpos(int xpos) {
+		this.xpos = xpos;
+	}
 
-public int getYpos() {
-	return ypos;
-}
+	public int getYpos() {
+		return ypos;
+	}
 
-public void setYpos(int ypos) {
-	this.ypos = ypos;
-}
-public String getDirection() {
-	return direction;
-}
+	public void setYpos(int ypos) {
+		this.ypos = ypos;
+	}
 
-public void setDirection(String direction) {
-	this.direction = direction;
-}
+	public String getDirection() {
+		return direction;
+	}
 
-String ToString () {
-	return name + "   " + point;
-}
-void addOnePoint() {
-	
-	point ++;
-}
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
 
-void subOnePoint() {
-	point --;
-}
-}
+	String ToString() {
+		return name + "   " + point;
+	}
 
+	void addOnePoint() {
+
+		point++;
+	}
+
+	void subOnePoint() {
+		point--;
+	}
+}
