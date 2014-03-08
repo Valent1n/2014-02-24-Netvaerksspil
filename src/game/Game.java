1362ac13d8c58516f12ef675f7b8575bffbcd081
kalleguld <kalleguld@gmail.com>
2014-03-08 02:30:48 +0100

    added prompt for server and port when stating game

--- src/game/Game.java
+++ src/game/Game.java
@@ -6,28 +6,31 @@ import java.util.ArrayList;
 
 public class Game {
 
-	/**
-	 * @param args
-	 *
-	 */
-	private static ArrayList<Player> players;
-	private static Player me;
-	
 	public static void main(String[] args) throws Exception {
 	
-		System.out.println("Indtast dit spillernavn");
+		GamePlayer g  = new GamePlayer();
+		Network network;
+		System.out.print("Indtast dit spillernavn: ");
 		BufferedReader b = new BufferedReader (new InputStreamReader(System.in));
 		String username = b.readLine();
 
-//		System.out.println("Indtast Servernavn:");
-//		String serverName = b.readLine();
+		System.out.println("Indtast Servernavn, eller enter for localhost:");
+		String serverName = b.readLine();
+		
+		if (serverName.equals("")) {
+			network = new Network(username, g);
+		} else {
+			Integer serverPort = null;
+			while (serverPort == null) {
+				System.out.print("Indtast portnummer: ");
+				String portString = b.readLine();
+				try {
+					serverPort = Integer.parseInt(portString);
+				} catch (Exception e) {}
+			}
+			network = new Network(username, serverName, serverPort, g);
+		}
 
-//		players = new ArrayList<Player>();
-//		me = new Player(username, -1);
-//		players.add(me);
-
-		GamePlayer g = new GamePlayer();
-		Network network = new Network(username, g);
 		g.setNetwork(network);
 	}
 
